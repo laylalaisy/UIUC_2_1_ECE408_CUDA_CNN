fe990c701d7100bc73c73369c3357b7394e9375e
Yaxin Peng <yaxinp2@illinois.edu>
2019-12-14 17:59:36 -0600

    two layers

--- ece408_src/new-forward_5_twoLayers.cpp
+++ ece408_src/new-forward_5_twoLayers.cpp
@@ -148,57 +148,6 @@ __global__ void ConvLayerForward2(int C, int K, int M, int H, int W, int W_out, 
     }
 }
 
-__global__ void forward_kernel1(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
-{
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    int W_grid = ceil((float)W_out/TILE_WIDTH_1);
-
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-    int bz = blockIdx.z;
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-    int tz = threadIdx.z;
-
-    int b = bx;
-    int m = by;
-    int h = (bz / W_grid) * TILE_WIDTH_1 + ty;
-    int w = (bz % W_grid) * TILE_WIDTH_1 + tx;
-
-	float acc = 0.0;
-
-    // Boundary check;
-    if (b < B && m < M && h < H_out && w < W_out) {
-		for (int c = 0; c < C; c++) {
-        	for (int p = 0; p < K; p++) {
-          		for (int q = 0; q < K; q ++) {
-            		acc += (x4d(b,c,h+p,w+q) * k4d(m,c,p,q));
-        		}
-      		}
-		}
-		y4d(b, m, h, w) = acc;
-    }
-    
-
-#undef y4d
-#undef x4d
-#undef k4d
-}
-
 /* 
    This function is called by new-inl.h
    Any code you write should be executed by this function.
@@ -237,7 +186,7 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     	dim3 gridDim(ceil(H_out*W_out/(1.0*TILE_WIDTH_1)),ceil(M/(1.0*TILE_WIDTH_1)),B);
     	dim3 blockDim(TILE_WIDTH_1,TILE_WIDTH_1,1);
     
-    	forwardKernel1<<<gridDim, blockDim, 0, s>>>(y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
+    	ConvLayerForward1<<<gridDim, blockDim, 0, s>>>(C, K, M, H, W, W_out, H_out, x.dptr_, w.dptr_, y.dptr_);
 
     	MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
     }
